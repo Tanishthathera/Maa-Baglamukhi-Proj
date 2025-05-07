eba7ccd57abe383b43481be70fe2a142aede17d2
Tanish thathera <tanishthathera@gmail.com>
2025-05-08 01:43:09 +0530

    fix: email temp px22

--- backend/index.js
+++ backend/index.js
@@ -10,34 +10,14 @@ const port = process.env.PORT || 3000;
 
 // Set CORS options
 const corsOptions = {
-  origin: "https://maabaglamukhi.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: "*",
+  origin: "https://maabaglamukhi.vercel.app", // Allow request onely from this domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP Methodes
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed Header
   credentials: true,
 };
 
-// Logging middleware
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  console.log("Headers:", req.headers);
-  next();
-});
-
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-
-// Handle OPTIONS requests globally
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", corsOptions.origin);
-    res.header("Access-Control-Allow-Methods", corsOptions.methods.join(","));
-    res.header("Access-Control-Allow-Headers", corsOptions.allowedHeaders);
-    res.header("Access-Control-Allow-Credentials", "true");
-    return res.sendStatus(204);
-  }
-  next();
-});
-
 app.use("/contact", contactUsRoute);
 
 app.listen(port, () => {
